2b3c0dc89c6f7199dfc3dccf316f12e98c4be291
Kaisar <kineticstishi19@gmail.com>
2023-11-21 13:27:49 +0300

    Added Exported .sql file from phpMyAdmin GUI

--- login_sample_db.sql
+++ login_sample_db.sql
@@ -1,7 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
---
+--s
 -- Host: 127.0.0.1
 -- Generation Time: Nov 21, 2023 at 11:23 AM
 -- Server version: 10.4.28-MariaDB
